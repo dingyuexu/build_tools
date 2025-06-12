79d827fa5d6490517176184d52cf3364688eda61
许定跃 <454293750@qq.com>
2025-06-12 23:20:37 +0800

    修改git clone問題

--- tools/linux/deps.py
+++ tools/linux/deps.py
@@ -48,7 +48,7 @@ def install_deps():
   for package in packages:
     base.cmd("sudo", ["apt-get", "install", "-y", package], True)
 
-  base.cmd("sudo", ["git", "config", "--global", "http.postBuffer",524288000], True)
+  base.cmd("sudo", ["git", "config", "--global", "http.postBuffer","524288000"], True)
   base.cmd("export", ["GIT_TRACE_PACKET=1"], True)
   base.cmd("export", ["GIT_TRACE=1"], True)
   base.cmd("export", ["GIT_CURL_VERBOSE=1"], True)
